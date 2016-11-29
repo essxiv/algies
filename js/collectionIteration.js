58eedad75f930ac73fc2b6240a6382a74b82b7a9
eSsXiV <essxiv@gmail.com>
2016-11-28 19:14:34 -0800

    last of index

--- js/collectionIteration.js
+++ js/collectionIteration.js
@@ -1,9 +1,19 @@
-var _;
+var _ = {}
 
-_.first = function(array, index) {
+_.first = function (array, index) {
     if (n === undefined) {
         return array[0]
     } else {
         return array.slice(0, n)
+    }
+}
+
+_.last = function (array, index) {
+    if (n === undefined) {
+        return array[array.length-1]
+    } else if (n > array.length-1) {
+        return array
+    } else {
+        return array.slice(array.length-n, array.length)
     }
 }
