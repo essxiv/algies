0ba90549eff38dad899f65413753f0d14fb54c17
eSsXiV <essxiv@gmail.com>
2016-12-01 12:11:55 -0800

    refactored white spaces

--- js/collectionIteration.js
+++ js/collectionIteration.js
@@ -1,47 +1,47 @@
 var _ = {}
 
-_.first = function (array, index) {
-    if (n === undefined) {
+_.first = function(array, index) {
+    if(n === undefined) {
         return array[0]
     } else {
         return array.slice(0, n)
     }
 }
 
-_.last = function (array, index) {
-    if (n === undefined) {
+_.last = function(array, index) {
+    if(n === undefined) {
         return array[array.length-1]
-    } else if (n > array.length-1) {
+    } else if(n > array.length-1) {
         return array
     } else {
         return array.slice(array.length-n, array.length)
     }
 }
 
-_.indexOf = function (array, target) {
+_.indexOf = function(array, target) {
     var result = -1
     _.each(array, function (item, index) {
-        if (item === target && result === -1) {
+        if(item === target && result === -1) {
             result = index
         }
     })
     return result
 }
 
-_.filter = function (collection, test) {
+_.filter = function(collection, test) {
     var results = []
     _.each(collection, function(value) {
         // implement test method you choose
-        if (test(val)) {
+        if(test(val)) {
             results.push(value)
         }
     })
     return results
 }
 
-_.reject = function (collection, test) {
+_.reject = function(collection, test) {
     var results = []
-    _.each(collection, function (value) {
+    _.each(collection, function(value) {
         if(!test(value)) {
             results.push(value)
         }
@@ -49,31 +49,31 @@ _.reject = function (collection, test) {
     return results
 }
 
-_.uniq = function (array) {
+_.uniq = function(array) {
     var result = []
-    _.each(array, function (value) {
-        if (_.indexOf(result, value) === -1) {
+    _.each(array, function(value) {
+        if(_.indexOf(result, value) === -1) {
             result.push(value)
         }
     })
     return result
 }
 
-_.map = function (collection, iterator) {
+_.map = function(collection, iterator) {
     var result = []
-    _.each(collection, function (value, key, collection) {
+    _.each(collection, function(value, key, collection) {
         result.push(iterator(value, key, collection))
     })
     return result
 }
 
-_.pluck = function (collection, key) {
+_.pluck = function(collection, key) {
     return _.map(collection, function(item) {
         return item[key]
     })
 }
 
-_.reduce = function (collection, iterator, accumulator) {
+_.reduce = function(collection, iterator, accumulator) {
     var accumulator = accumulator
     _.each(collection, function (value) {
         if (accumulator === undefined) {
